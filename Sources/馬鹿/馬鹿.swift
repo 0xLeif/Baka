1450b90ea10b7427d9ffa52842f91514335e5453
0xLeif <zmeriksen@icloud.com>
2021-01-22 17:42:31 -0600

    馬鹿‼️

--- Sources/馬鹿/馬鹿.swift
+++ Sources/馬鹿/馬鹿.swift
@@ -1,3 +1,35 @@
-struct 馬鹿 {
-    var text = "Hello, World!"
+public typealias ストリング = String
+
+public enum 馬鹿レベル: ストリング {
+    case 軽い = "❕"
+    case 正常 = "❗️"
+    case とても = "‼️"
+}
+
+public struct 馬鹿 {
+    let レベル: 馬鹿レベル
+    let 説明: ストリング
+}
+
+public extension 馬鹿 {
+    func ログ() -> ストリング {
+        "\(レベル.rawValue): \(説明)"
+    }
+}
+
+public extension 馬鹿 {
+    // ❕
+    static func 軽い(説明: ストリング) -> Self {
+        馬鹿(レベル: .軽い, 説明: 説明)
+    }
+    
+    // ❗️
+    static func 正常(説明: ストリング) -> Self {
+        馬鹿(レベル: .正常, 説明: 説明)
+    }
+    
+    // ‼️
+    static func とても(説明: ストリング) -> Self {
+        馬鹿(レベル: .とても, 説明: 説明)
+    }
 }
--- Tests/馬鹿Tests/馬鹿Tests.swift
+++ Tests/馬鹿Tests/馬鹿Tests.swift
@@ -2,14 +2,38 @@ import XCTest
 @testable import 馬鹿
 
 final class 馬鹿Tests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(馬鹿().text, "Hello, World!")
+    func testプロジェクト() {
+        XCTAssertEqual(
+            馬鹿.とても(説明: "このプロジェクト").ログ(),
+            "‼️: このプロジェクト"
+        )
+    }
+    
+    func test軽い() {
+        XCTAssertEqual(
+            馬鹿.軽い(説明: "軽い").ログ(),
+            "❕: 軽い"
+        )
+    }
+    
+    func test正常() {
+        XCTAssertEqual(
+            馬鹿.正常(説明: "正常").ログ(),
+            "❗️: 正常"
+        )
+    }
+    
+    func testとても() {
+        XCTAssertEqual(
+            馬鹿.とても(説明: "とても").ログ(),
+            "‼️: とても"
+        )
     }
 
     static var allTests = [
-        ("testExample", testExample),
+        ("testプロジェクト", testプロジェクト),
+        ("test軽い", test軽い),
+        ("test正常", test正常),
+        ("testとても", testとても),
     ]
 }
